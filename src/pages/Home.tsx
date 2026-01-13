7108dfcf3a2f627ca41ecce00bd62a8fb9e72cc2
Le-Sourcier <hackersranch@gmail.com>
2026-01-13 15:49:16 +0000

    update/*

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -413,7 +413,7 @@ const Home = () => {
             </p>
           </motion.div>
 
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
             {featuredProjects.map((project, index) => (
               <motion.div
                 key={project.id}
@@ -510,7 +510,7 @@ const Home = () => {
             </p>
           </motion.div>
 
-          <div className="grid md:grid-cols-2 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-8">
             {featuredPosts.map((post, index) => (
               <motion.article
                 key={post.id}
